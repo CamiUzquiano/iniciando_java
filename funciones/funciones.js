60fc4d56445c1e033b093793288df500500e0f63
Camila Uzquiano <camila.uzquiano@ucb.edu.bo>
2025-03-24 10:52:00 -0400

    Funciones con java script

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -14,10 +14,16 @@
 let nombre = "Camila"; //let es un nombre reservado para otorgar a una variable, "nombre" es el nombre de la variable let y "Camila" es el valor que le estoy otorgando a esta variable
 console.log(nombre); //aqui pedimos que por la consola nos imprima el contenido de la variable nombre
 console.log(nombre.length); //aqui decimos que use la consola para imprimirme el numero de caractéres de la variable
-let nombre1, apellido, edad;
-nombre1 = prompt("¿Cuál es tu nombre?") //con el prompt se permite que el usuario meta información a una variable
-apellido = prompt("¿Cuál es tu apellido?")
-edad = prompt("¿Cuál es tu edad?")
-console.log("Mi nombre es "+nombre1+apellido+" y tengo "+edad+" años."); //con esta forma estoy encadenando los datos, o sea se van ligando
-console.log(`Mi nombre es ${nombre1} ${apellido} y tengo ${edad} años.`); //con los backticks `` hago una sintais más cómoda 
-console.log("Mi nombre es", nombre1, apellido, "y tengo", edad, "años.");
+
+//usamos en una función el llamar los datos generales de un usuario
+function datos(nombre1, apellido, edad){ //estos son los argumentos
+    console.log("Mi nombre es " + nombre1 + " " + apellido + " y tengo " + edad + " años."); //con esta forma estoy encadenando los datos, o sea se van ligando
+    console.log(`Mi nombre es ${nombre1} ${apellido} y tengo ${edad} años.`); //con los backticks `` hago una sintais más cómoda 
+    console.log("Mi nombre es", nombre1, apellido, "y tengo", edad, "años.");
+}
+
+// Pedimos los datos y los pasamos como argumentos
+let nombre1 = prompt("¿Cuál es tu nombre?"); //con el prompt se permite que el usuario meta información a una variable
+let apellido = prompt("¿Cuál es tu apellido?");
+let edad = prompt("¿Cuál es tu edad?");
+datos(nombre1, apellido, edad); //llamamos a la función para que se ejecute
